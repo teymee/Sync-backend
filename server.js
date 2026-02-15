871b7429257fe9d0063743c7a891db65760ec45d
teymee <teymee17@gmail.com>
2026-02-15 17:13:22 +0100

    update

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ import express from "express";
 import cors from "cors";
 import fetch from "node-fetch";
 import dotenv from "dotenv";
-import { APIs } from "./api";
+import { APIs } from "./api.js";
 
 dotenv.config();
 
